7f33ae20241c50cf98145e14aca448871fb7c2b7
Talha Iqbal <talhaiqbal801@gamil.com>
2020-12-28 21:01:09 +0500

    Configured mongoose to use new URL parser

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 //connect to mongoDB
 // const db = 'mongodb://localhost:27017/smart-shopping-list';
 const db = 'mongodb+srv://crazydevz:Dezong%4041@cluster0.5j8tc.mongodb.net/smart-shopping-list?retryWrites=true'
+mongoose.set('useNewUrlParser', true);
 mongoose.promise = global.promise;
 
 // connect mongoose to database
